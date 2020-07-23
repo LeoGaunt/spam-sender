79fd61a4688670c8c4a2786cea88d2552aad7666
Leo Gaunt <leogaunt05@outlook.com>
2020-07-23 20:06:09 +0100

    added seperate files for each OS

---
+++ windows/send.py
@@ -0,0 +1,23 @@
+import pyperclip
+import pyautogui
+import time
+
+#Gives time to get to application
+time.sleep(5)
+
+#Words to be copied
+lyrics = "Insert Lyrics/Script here"
+
+#Splits long text into words
+split = lyrics.split()
+
+#loops pressing buttons
+for x in range(0,len(split)):
+    pyperclip.copy(split[x])
+    pyautogui.keyDown('control')
+    pyautogui.keyDown('v')
+    pyautogui.keyUp('control')
+    pyautogui.keyUp('v')
+    pyautogui.keyDown('enter')
+    pyautogui.keyUp('enter')
+    time.sleep(0.1)
